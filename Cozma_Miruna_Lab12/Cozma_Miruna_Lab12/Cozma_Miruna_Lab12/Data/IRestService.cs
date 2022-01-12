3b1a214f2cb45a252c1b371e197ed330fd85f0d4
Miruna Cozma <bianca.mirunacozma@gmail.com>
2022-01-12 17:05:11 +0200

    Add project files.

---
+++ Cozma_Miruna_Lab12/Cozma_Miruna_Lab12/Cozma_Miruna_Lab12/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cozma_Miruna_Lab12.Models;
+
+namespace Cozma_Miruna_Lab12.Data
+{
+    
+    
+        public interface IRestService
+        {
+            Task<List<ShopList>> RefreshDataAsync();
+            Task SaveShopListAsync(ShopList item, bool isNewItem);
+            Task DeleteShopListAsync(int id);
+        }
+    
+}
